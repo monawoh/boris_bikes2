862f31502e4913f12d346bc93734c6ea54aaaa98
Monaliza <mo.o@hotmail.co.uk>
2021-10-19 18:03:59 +0100

    challenge 5 incomplete

---
+++ lib/boris_bikes.rb
@@ -0,0 +1,4 @@
+# class boris_bikes
+#     def dockingstation(number)
+#         number.push(dockingstation)
+#     end
---
+++ spec/spec.rb
@@ -0,0 +1,7 @@
+# require 'docking_station' 
+# it 'somewhere that has bikes' do
+#     describe DockingStation do 
+#         docking_station = DockingStation.new
+#         expect{ docking_station.number('1') }.not_to raise_error 
+#     end
+# end
