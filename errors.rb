bbbf21ed3b32c49690fcd490ba3c45e6f707d62d
Jacob <jacobchanrahan@gmail.com>
2021-10-19 17:21:22 +0100

    Errors

---
+++ errors.rb
@@ -0,0 +1,14 @@
+# 3.0.0 :001 > docking_station = DockingStation.new
+# Traceback (most recent call last):
+#         4: from /Users/jacobhanrahan/.rvm/rubies/ruby-3.0.0/bin/irb:23:in `<main>'
+#         3: from /Users/jacobhanrahan/.rvm/rubies/ruby-3.0.0/bin/irb:23:in `load'
+#         2: from /Users/jacobhanrahan/.rvm/rubies/ruby-3.0.0/lib/ruby/gems/3.0.0/gems/irb-1.3.0/exe/irb:11:in `<top (required)>'
+#         1: from (irb):1:in `<main>'
+# NameError (uninitialized constant DockingStation)
+
+
+#  Write down the type of error => Uninitialized constant
+#  Write down the file path where the error happened => /Users/jacobhanrahan/.rvm/rubies/ruby-3.0.0/lib/ruby/gems/3.0.0/gems/irb-1.3.0/exe/irb:11:in
+#  Write down the line number of the error => line 5
+#  Use the Ruby Documentation to find out what the error means => class has not been initialized, no file linked to 
+#  Suggest one way of solving the error. => Create a file that defines the Class and method 
